c6084ab566256be06c906d7d8bb3ba35ed8a0b33
Dust07 <tienphat0707@gmail.com>
2022-06-06 08:22:14 +0700

    Init Movie Component

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
     <HeroComponent />
+
+    <MovieComponent :movies="movies" />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
   name: "HomePage",
+
   data() {
     return {
       movies: [],
@@ -19,9 +23,7 @@ export default {
   methods: {
     async getMovie() {
       const response = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=2d092c05b6df26808e64c2b3859bdd49&language=en-US&page=1")
-      const results = await response;
-      results.data.results.forEach((result) => this.movies.push(result));
-      console.log(this.movies)
+      response.data.results.forEach((result) => this.movies.push(result));
     }
   }
 }
